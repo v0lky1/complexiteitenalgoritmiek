dc749df8509083463f9d776bc180c3896a436d73
v0lky1 <remco.volkers1@gmail.com>
2020-01-13 22:33:03 +0100

    added a menu for the program, we need to fix removeLow

--- src/Opdracht2/Main.java
+++ src/Opdracht2/Main.java
@@ -1,5 +1,8 @@
 package Opdracht2;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,38 +11,44 @@ public class Main {
 
     private void run() {
         IntervalHeap queue = new IntervalHeap();
+        Scanner listener = new Scanner(System.in);
+        buildMethodsMenu();
+        while (true) {
+            System.out.println("Enter your choice: (1=add, 2=removehigh, 3=removelow, 4=currmax, 5=currmin, 6=currsize, 7=isEmpty?)");
 
-        System.out.println("Size: " + queue.size());
-        queue.add(5);
-        System.out.println("Size: " + queue.size());
-        System.out.println("Min: " + queue.getLow());
-        System.out.println("Max: " + queue.getHigh());
-        queue.add(3);
-        System.out.println("Size: " + queue.size());
-        System.out.println("Min: " + queue.getLow());
-        System.out.println("Max: " + queue.getHigh());
-        queue.add(8);
-        queue.add(11);
-        System.out.println("Size: " + queue.size());
-        System.out.println("Min: " + queue.getLow());
-        System.out.println("Max: " + queue.getHigh());
-        queue.add(18);
-        queue.add(13);
-        System.out.println("Size: " + queue.size());
-        System.out.println("Min: " + queue.getLow());
-        System.out.println("Max: " + queue.getHigh());
-        System.out.println("Min: " + queue.removeLow());
-        System.out.println("Min: " + queue.getLow());
-        System.out.println("High: " + queue.removeMax());
-        System.out.println("Size: " + queue.size());
-        System.out.println("High: " + queue.removeMax());
-        System.out.println("Size: " + queue.size());
-        System.out.println("High: " + queue.removeMax());
-        System.out.println("Size: " + queue.size());
-        System.out.println("High: " + queue.removeMax());
-        System.out.println("Size: " + queue.size());
-        System.out.println("Removing max: " + queue.removeMax());
-        System.out.println("Size: " + queue.size());
-        System.out.println("List empty: " + queue.isEmpty());
+            int myChoice = listener.nextInt();
+
+            if (myChoice == 0) {
+                break;
+            } else if (myChoice == 1) {
+                System.out.println("Number to add: ");
+                queue.add(listener.nextInt());
+            } else if (myChoice == 2) {
+                System.out.println("Removing highest number: " + queue.removeMax());
+            } else if (myChoice == 3) {
+                System.out.println("Removing lowest number: " + queue.removeLow());
+            } else if (myChoice == 4) {
+                System.out.println("Current max value is: " + queue.getHigh());
+            } else if(myChoice == 5){
+                System.out.println("Current min value is: " + queue.getLow());
+            } else if(myChoice == 6){
+                System.out.println("Current queue size is: " + queue.size());
+            } else if (myChoice ==7){
+                System.out.println("List is empty?: " + queue.isEmpty());
+            }
+        }
+    }
+
+    private void buildMethodsMenu() {
+        System.out.println("***** METHODS ***** \n" +
+                "What do you want to do?\n" +
+                "1) Add a number \n" +
+                "2) Remove the highest number \n" +
+                "3) Remove the lowest number \n" +
+                "4) Get the max value \n" +
+                "5) Get the min value \n" +
+                "6) Get the number of values \n" +
+                "7) Check if list is empty \n" +
+                "0) Exit");
     }
 }
